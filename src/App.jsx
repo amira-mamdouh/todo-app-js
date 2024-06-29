4acea05e93762a7eb4b929e98147d879af29a33f
unknown <amiramamdouh320@gmail.com>
2024-06-29 16:02:23 +0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,24 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import ArabicApp from "./Components/ArapicApp/ArabicApp";
 import EnglishApp from "./Components/EnglishApp/EnglishApp";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/arabic",
-      element: <ArabicApp />,
-    },
-    {
-      path: "/english",
-      element: <EnglishApp />,
-    },
-  ],
+const router = createBrowserRouter([
   {
-    basename: "/english",
-  }
-);
+    path: "/arabic",
+    element: <ArabicApp />,
+  },
+  {
+    path: "/",
+    element: <EnglishApp />,
+  },
+  {
+    path: "*",
+    element: <EnglishApp />,
+  },
+  {
+    path: "/english",
+    element: <EnglishApp />,
+  },
+]);
 
 export default function App() {
   return <RouterProvider router={router}></RouterProvider>;
